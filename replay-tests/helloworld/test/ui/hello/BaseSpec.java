4a2ca4f72f95a66c18922d3f61fe6cc008b05412
Andrei Solntsev <andrei.solntsev@gmail.com>
2017-11-14 00:26:38 +0200

    add RePlay integration tests

---
+++ replay-tests/helloworld/test/ui/hello/BaseSpec.java
@@ -0,0 +1,37 @@
+package ui.hello;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import play.Play;
+import play.server.Server;
+
+import java.io.File;
+
+import static org.openqa.selenium.net.PortProber.findFreePort;
+
+public class BaseSpec {
+  @Before
+  public void setUp() throws InterruptedException {
+    Thread playStarter = new Thread(() -> {
+      Play.init(new File(System.getProperty("application.path", ".")), "test");
+
+      int port = findFreePort();
+      new Server(new String[]{"--http.port=" + port});
+
+      Configuration.baseUrl = "http://localhost:" + port;
+      Play.configuration.setProperty("application.baseUrl", Configuration.baseUrl);
+    }, "Play! starter thread");
+
+    playStarter.start();
+    playStarter.join();
+
+    Configuration.browser = "chrome";
+    Configuration.headless = true;
+  }
+
+  @After
+  public void tearDown() {
+    Play.stop();
+  }
+}
---
+++ replay-tests/helloworld/test/ui/hello/HelloWorldSpec.java
@@ -0,0 +1,15 @@
+package ui.hello;
+
+import org.junit.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class HelloWorldSpec extends BaseSpec {
+  @Test
+  public void name() {
+    open("/");
+    $("h1").shouldHave(text("Hello, world!"));
+  }
+}
