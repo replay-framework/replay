be489cfc05791e5c4373057c955a4366158e4983
Andrei Solntsev, Elina Matvejeva <andrei@codeborne.com, elina@codeborne.com>
2017-11-13 17:48:41 +0200

    remove bouncycastle dependency
    * remove support for running Play on https
    * also remove the whole `play.server.ssl` package

--- framework/src/play/server/Server.java
+++ framework/src/play/server/Server.java
@@ -7,7 +7,6 @@ import play.Logger;
 import play.Play;
 import play.Play.Mode;
 import play.libs.IO;
-import play.server.ssl.SslHttpServerPipelineFactory;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
@@ -19,7 +18,6 @@ import java.util.concurrent.Executors;
 public class Server {
 
     public static int httpPort;
-    public static int httpsPort;
 
     public static final String PID_FILE = "server.pid";
 
@@ -28,20 +26,10 @@ public class Server {
         System.setProperty("file.encoding", "utf-8");
         Properties p = Play.configuration;
 
-        httpPort = Integer.parseInt(getOpt(args, "http.port", p.getProperty("http.port", "-1")));
-        httpsPort = Integer.parseInt(getOpt(args, "https.port", p.getProperty("https.port", "-1")));
-
-        if (httpPort == -1 && httpsPort == -1) {
-            httpPort = 9000;
-        }
-
-        if (httpPort == httpsPort) {
-            Logger.error("Could not bind on https and http on the same port " + httpPort);
-            Play.fatalServerErrorOccurred();
-        }
+        httpPort = Integer.parseInt(getOpt(args, "http.port", p.getProperty("http.port", "9000")));
 
         InetAddress address = null;
-        InetAddress secureAddress = null;
+
         try {
             if (p.getProperty("http.address") != null) {
                 address = InetAddress.getByName(p.getProperty("http.address"));
@@ -53,40 +41,28 @@ public class Server {
             Logger.error(e, "Could not understand http.address");
             Play.fatalServerErrorOccurred();
         }
-        try {
-            if (p.getProperty("https.address") != null) {
-                secureAddress = InetAddress.getByName(p.getProperty("https.address"));
-            } else if (System.getProperties().containsKey("https.address")) {
-                secureAddress = InetAddress.getByName(System.getProperty("https.address"));
-            }
-        } catch (Exception e) {
-            Logger.error(e, "Could not understand https.address");
-            Play.fatalServerErrorOccurred();
-        }
+
         ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
                 Executors.newCachedThreadPool(), Executors.newCachedThreadPool())
         );
         try {
-            if (httpPort != -1) {
-                bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
+            bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
 
-                bootstrap.bind(new InetSocketAddress(address, httpPort));
-                bootstrap.setOption("child.tcpNoDelay", true);
+            bootstrap.bind(new InetSocketAddress(address, httpPort));
+            bootstrap.setOption("child.tcpNoDelay", true);
 
-                if (Play.mode == Mode.DEV) {
-                    if (address == null) {
-                        Logger.info("Listening for HTTP on port %s (Waiting a first request to start) ...", httpPort);
-                    } else {
-                        Logger.info("Listening for HTTP at %2$s:%1$s (Waiting a first request to start) ...", httpPort, address);
-                    }
+            if (Play.mode == Mode.DEV) {
+                if (address == null) {
+                    Logger.info("Listening for HTTP on port %s (Waiting a first request to start) ...", httpPort);
                 } else {
-                    if (address == null) {
-                        Logger.info("Listening for HTTP on port %s ...", httpPort);
-                    } else {
-                        Logger.info("Listening for HTTP at %2$s:%1$s  ...", httpPort, address);
-                    }
+                    Logger.info("Listening for HTTP at %2$s:%1$s (Waiting a first request to start) ...", httpPort, address);
                 }
-
+            } else {
+                if (address == null) {
+                    Logger.info("Listening for HTTP on port %s ...", httpPort);
+                } else {
+                    Logger.info("Listening for HTTP at %2$s:%1$s  ...", httpPort, address);
+                }
             }
 
         } catch (ChannelException e) {
@@ -94,38 +70,8 @@ public class Server {
             Play.fatalServerErrorOccurred();
         }
 
-        bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
-                Executors.newCachedThreadPool(), Executors.newCachedThreadPool())
-        );
-
-        try {
-            if (httpsPort != -1) {
-                bootstrap.setPipelineFactory(new SslHttpServerPipelineFactory());
-                bootstrap.bind(new InetSocketAddress(secureAddress, httpsPort));
-                bootstrap.setOption("child.tcpNoDelay", true);
-
-                if (Play.mode == Mode.DEV) {
-                    if (secureAddress == null) {
-                        Logger.info("Listening for HTTPS on port %s (Waiting a first request to start) ...", httpsPort);
-                    } else {
-                        Logger.info("Listening for HTTPS at %2$s:%1$s (Waiting a first request to start) ...", httpsPort, secureAddress);
-                    }
-                } else {
-                    if (secureAddress == null) {
-                        Logger.info("Listening for HTTPS on port %s ...", httpsPort);
-                    } else {
-                        Logger.info("Listening for HTTPS at %2$s:%1$s  ...", httpsPort, secureAddress);
-                    }
-                }
-
-            }
-
-        } catch (ChannelException e) {
-            Logger.error("Could not bind on port " + httpsPort, e);
-            Play.fatalServerErrorOccurred();
-        }
         if (Play.mode == Mode.DEV || Play.runingInTestMode()) {
-           // print this line to STDOUT - not using logger, so auto test runner will not block if logger is misconfigured (see #1222)     
+           // print this line to STDOUT - not using logger, so auto test runner will not block if logger is misconfigured (see #1222)
            System.out.println("~ Server is up and running");
         }
     }
