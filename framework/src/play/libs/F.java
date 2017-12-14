94058b081be27f183de495bfacb61f3c299f1e94
Andrei Solntsev <andrei@codeborne.com>
2017-12-14 11:39:14 +0200

    remove class F.Tuple

--- framework/src/play/libs/F.java
+++ framework/src/play/libs/F.java
@@ -77,20 +77,4 @@ public class F {
             }
         }
     }
-
-    public static class Tuple<A, B> {
-
-        public final A _1;
-        public final B _2;
-
-        public Tuple(A _1, B _2) {
-            this._1 = _1;
-            this._2 = _2;
-        }
-
-        @Override
-        public String toString() {
-            return "T2(_1: " + _1 + ", _2: " + _2 + ")";
-        }
-    }
 }
