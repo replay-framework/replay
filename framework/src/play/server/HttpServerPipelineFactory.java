925926b9347597bf89cb9f1aec090cf4f46188da
Andrei Solntsev <andrei.solntsev@gmail.com>
2018-03-11 15:57:56 +0200

    simplify HttpServerPipelineFactory
    nobody ever used setting "play.netty.pipeline" - no need to customize netty internals

--- framework/src/play/server/HttpServerPipelineFactory.java
+++ framework/src/play/server/HttpServerPipelineFactory.java
@@ -3,79 +3,43 @@ package play.server;
 import org.jboss.netty.channel.ChannelHandler;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import play.Play;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.jboss.netty.channel.Channels.pipeline;
+import org.jboss.netty.channel.DefaultChannelPipeline;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+import org.jboss.netty.handler.stream.ChunkedWriteHandler;
+import play.Invoker;
 
 public class HttpServerPipelineFactory implements ChannelPipelineFactory {
-    private static final Logger logger = LoggerFactory.getLogger(HttpServerPipelineFactory.class);
+    private final Invoker invoker;
 
-    private String pipelineConfig = Play.configuration.getProperty("play.netty.pipeline", "play.server.FlashPolicyHandler,org.jboss.netty.handler.codec.http.HttpRequestDecoder,play.server.StreamChunkAggregator,org.jboss.netty.handler.codec.http.HttpResponseEncoder,org.jboss.netty.handler.stream.ChunkedWriteHandler,play.server.PlayHandler");
-
-    protected static Map<String, Class> classes = new HashMap<>();
+    public HttpServerPipelineFactory(Invoker invoker) {
+        this.invoker = invoker;
+    }
 
     @Override
-    public ChannelPipeline getPipeline() throws Exception {
-
-        ChannelPipeline pipeline = pipeline();
-        
-        String[] handlers = pipelineConfig.split(",");  
-        if(handlers.length <= 0){
-            logger.error("You must defined at least the playHandler in \"play.netty.pipeline\"");
-            return pipeline;
-        }       
-        
-        // Create the play Handler (always the last one)
-        String handler = handlers[handlers.length - 1];
-        ChannelHandler instance = getInstance(handler);
-        PlayHandler playHandler = (PlayHandler) instance;
-        if (playHandler == null) {
-            logger.error("The last handler must be the playHandler in \"play.netty.pipeline\"");
-            return pipeline;
-        }
-      
-        // Get all the pipeline. Give the user the opportunity to add their own
-        for (int i = 0; i < handlers.length - 1; i++) {
-            handler = handlers[i];
-            try {
-                String name = getName(handler.trim());
-                instance = getInstance(handler);
-                if (instance != null) {
-                    pipeline.addLast(name, instance);
-                    playHandler.pipelines.put(name, instance);
-                }
-            } catch (Throwable e) {
-                logger.error(" error adding {}", handler, e);
-            }
-        }
-               
-        pipeline.addLast("handler", playHandler);
-        playHandler.pipelines.put("handler", playHandler);
-
-        return pipeline;
+    public ChannelPipeline getPipeline() {
+        return new PlayChannelPipeline();
     }
 
-    protected String getName(String name) {
-        if (name.lastIndexOf('.') > 0)
-            return name.substring(name.lastIndexOf('.') + 1);
-        return name;
-    }
+    class PlayChannelPipeline extends DefaultChannelPipeline {
+        private final PlayHandler playHandler = new PlayHandler(invoker);
 
-    protected ChannelHandler getInstance(String name) throws Exception {
+        PlayChannelPipeline() {
+            addChannelHandler(new FlashPolicyHandler());
+            addChannelHandler(new HttpRequestDecoder());
+            addChannelHandler(new StreamChunkAggregator());
+            addChannelHandler(new HttpResponseEncoder());
+            addChannelHandler(new ChunkedWriteHandler());
 
-        Class clazz = classes.get(name);
-        if (clazz == null) {
-            clazz = Class.forName(name);
-            classes.put(name, clazz);
+            addLast("handler", playHandler);
+            playHandler.pipelines.put("handler", playHandler);
         }
-        if (ChannelHandler.class.isAssignableFrom(clazz))
-            return (ChannelHandler)clazz.newInstance(); 
-        return null;
+
+        private void addChannelHandler(ChannelHandler instance) {
+            String name = instance.getClass().getSimpleName();
+            addLast(name, instance);
+            playHandler.pipelines.put(name, instance);
+        }
     }
 }
 
