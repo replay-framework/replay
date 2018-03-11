6ce2de809a2430ec356e79e9915e56532cdab4f0
Andrei Solntsev <andrei.solntsev@gmail.com>
2018-03-11 17:51:52 +0200

    merge RebelController into play.mvc.Controller

--- framework/src/play/mvc/Controller.java
+++ framework/src/play/mvc/Controller.java
@@ -1,4 +1,47 @@
 package play.mvc;
 
+import play.data.validation.Validation;
+import play.mvc.results.BadRequest;
+import play.mvc.results.Forbidden;
+import play.rebel.View;
+
 public class Controller implements PlayController {
+  protected Http.Request request = Http.Request.current();
+  protected Http.Response response = Http.Response.current();
+  protected Scope.Session session = Scope.Session.current();
+  protected Scope.Flash flash = Scope.Flash.current();
+  protected Scope.Params params = Scope.Params.current();
+  protected Scope.RenderArgs renderArgs = Scope.RenderArgs.current();
+  protected Validation validation = Validation.current();
+
+  protected void checkAuthenticity() {
+    if (params.get("authenticityToken") == null
+      || !params.get("authenticityToken").equals(session.getAuthenticityToken())) {
+      throw new Forbidden("Bad authenticity token");
+    }
+  }
+
+  protected static void forbidden() {
+    throw new Forbidden("Access denied");
+  }
+
+  protected static void forbidden(String reason) {
+    throw new Forbidden(reason);
+  }
+
+  protected BadRequest badRequest() {
+    return new BadRequest("Bad request");
+  }
+
+  protected View viewResult() {
+    return new View();
+  }
+
+  protected View viewResult(String templateName) {
+    return new View(templateName);
+  }
+
+  protected Forbidden forbiddenResult() {
+    return new Forbidden();
+  }
 }
--- framework/src/play/rebel/RebelController.java
+++ framework/src/play/rebel/RebelController.java
@@ -1,53 +1,6 @@
 package play.rebel;
 
-import play.data.validation.Validation;
-import play.mvc.Http;
-import play.mvc.PlayController;
-import play.mvc.Scope;
-import play.mvc.results.BadRequest;
-import play.mvc.results.Forbidden;
+import play.mvc.Controller;
 
-/**
- * A superclass for all controllers that are intended to work without play enhancers.
- */
-public class RebelController implements PlayController {
-
-  protected Http.Request request = Http.Request.current();
-  protected Http.Response response = Http.Response.current();
-  protected Scope.Session session = Scope.Session.current();
-  protected Scope.Flash flash = Scope.Flash.current();
-  protected Scope.Params params = Scope.Params.current();
-  protected Scope.RenderArgs renderArgs = Scope.RenderArgs.current();
-  protected Validation validation = Validation.current();
-
-  protected void checkAuthenticity() {
-    if (params.get("authenticityToken") == null
-        || !params.get("authenticityToken").equals(session.getAuthenticityToken())) {
-      throw new Forbidden("Bad authenticity token");
-    }
-  }
-
-  protected static void forbidden() {
-    throw new Forbidden("Access denied");
-  }
-
-  protected static void forbidden(String reason) {
-    throw new Forbidden(reason);
-  }
-
-  protected BadRequest badRequest() {
-    return new BadRequest("Bad request");
-  }
-
-  protected View viewResult() {
-    return new View();
-  }
-
-  protected View viewResult(String templateName) {
-    return new View(templateName);
-  }
-
-  protected Forbidden forbiddenResult() {
-    return new Forbidden();
-  }
+public class RebelController extends Controller {
 }
