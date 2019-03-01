cc821d586ae7b9c865215359986980915a41bc94
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-03-01 10:38:51 +0200

    add flash message to redirect

--- framework/src/play/mvc/results/Redirect.java
+++ framework/src/play/mvc/results/Redirect.java
@@ -11,16 +11,20 @@ import play.mvc.Url;
 
 import java.util.Map;
 
+import static java.util.Collections.emptyMap;
+
 /**
  * 302 Redirect
  */
 public class Redirect extends Result {
 
     public String url;
+    private final Map<String, String> flashMessages;
     public int code = Http.StatusCode.FOUND;
 
     public Redirect(String url) {
         this.url = url;
+        this.flashMessages = emptyMap();
     }
 
     /**
@@ -32,11 +36,17 @@ public class Redirect extends Result {
      *            Parameters to be included at the end of the URL as a HTTP GET. This is a map whose entries are written out as key1=value1&amp;key2=value2 etc..
      */
     public Redirect(String url, Map<String, Object> parameters) {
+        this(url, parameters, emptyMap());
+    }
+
+    public Redirect(String url, Map<String, Object> parameters, Map<String, String> flashMessages) {
         this.url = new Url(url, parameters).toString();
+        this.flashMessages = flashMessages;
     }
 
     public Redirect(String url,boolean permanent) {
         this.url = url;
+        this.flashMessages = emptyMap();
         if (permanent)
             this.code = Http.StatusCode.MOVED;
     }
@@ -44,12 +54,15 @@ public class Redirect extends Result {
     public Redirect(String url,int code) {
         this.url = url;
         this.code=code;
+        this.flashMessages = emptyMap();
     }
 
     @Override
     public void apply(Request request, Response response, Session session, RenderArgs renderArgs, Flash flash) {
         try {
-            // do not touch any valid uri: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.30 
+            this.flashMessages.forEach((key, value) -> flash.put(key, value));
+
+            // do not touch any valid uri: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.30
             if (url.matches("^\\w+://.*")) {
             } else if (url.startsWith("/")) {
                 url = String.format("http%s://%s%s%s", request.secure ? "s" : "", request.domain, (request.port == 80 || request.port == 443) ? "" : ":" + request.port, url);
