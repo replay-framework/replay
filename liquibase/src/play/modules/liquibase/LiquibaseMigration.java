bf3a9426bf78d99f338ae76591b859b90cb749bc
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-04-14 09:37:21 +0300

    speed-up application startup by restoring DB from previously stored dump (currently only for H2 - just in case)

--- liquibase/src/play/modules/liquibase/LiquibaseMigration.java
+++ liquibase/src/play/modules/liquibase/LiquibaseMigration.java
@@ -12,10 +12,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.Play;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static java.lang.Boolean.parseBoolean;
 import static java.lang.System.nanoTime;
@@ -25,14 +27,18 @@ public final class LiquibaseMigration {
 
   private static final Logger logger = LoggerFactory.getLogger(LiquibaseMigration.class);
 
+  private final String dbName;
   private final String changeLogPath;
+  private final File dumpFile;
   private final String driver;
   private final String url;
   private final String username;
   private final String password;
 
-  public LiquibaseMigration(String changeLogPath, String driver, String url, String username, String password) {
+  public LiquibaseMigration(String dbName, String changeLogPath, String dumpFile, String driver, String url, String username, String password) {
+    this.dbName = dbName;
     this.changeLogPath = changeLogPath;
+    this.dumpFile = new File(dumpFile);
     this.driver = driver;
     this.url = url;
     this.username = username;
@@ -50,25 +56,59 @@ public final class LiquibaseMigration {
     long start = nanoTime();
 
     try (Connection cnx = getConnection()) {
-      Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(cnx));
-      if (cnx.getMetaData().getURL().contains(":h2:")) {
+      if (isH2()) {
+        restoreFromDump(cnx);
         LockServiceFactory.getInstance().register(new NonLockingLockService());
       }
 
+      Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(cnx));
       try {
         ResourceAccessor accessor = new DuplicatesIgnoringResourceAccessor(Thread.currentThread().getContextClassLoader());
         Liquibase liquibase = new Liquibase(changeLogPath, accessor, database);
         liquibase.update(Play.configuration.getProperty("liquibase.contexts", ""));
+        if (isH2()) {
+          storeDump(cnx);
+        }
       }
       finally {
         close(database);
       }
+
+      logger.info("{} finished in {} ms.", changeLogPath, NANOSECONDS.toMillis(nanoTime() - start));
     }
     catch (SQLException | LiquibaseException sqe) {
       throw new LiquibaseUpdateException("Failed to migrate " + changeLogPath, sqe);
     }
-    finally {
-      logger.info("{} finished in {} ms.", changeLogPath, NANOSECONDS.toMillis(nanoTime() - start));
+  }
+
+  private boolean isH2() {
+    return url.contains(":h2:");
+  }
+
+  private void storeDump(Connection cnx) {
+    logger.info("Store {} DB to {}", dbName, dumpFile);
+    try (Statement statement = cnx.createStatement()) {
+      String sql = String.format("script nopasswords to '%s'", dumpFile);
+      statement.execute(sql);
+    }
+    catch (SQLException ex) {
+      throw new LiquibaseUpdateException(String.format("Failed to store %s DB dump to %s", dbName, dumpFile), ex);
+    }
+  }
+
+  private void restoreFromDump(Connection cnx) {
+    if (dumpFile.exists()) {
+      logger.info("Restore {} DB from {}", dbName, dumpFile);
+      try (Statement statement = cnx.createStatement()) {
+        String sql = String.format("runscript from '%s'", dumpFile);
+        statement.execute(sql);
+      }
+      catch (SQLException ex) {
+        throw new LiquibaseUpdateException(String.format("Failed to restore %s DB from dump %s", dbName, dumpFile), ex);
+      }
+    }
+    else {
+      logger.info("{} DB dump {} not found, creating DB from scratch", dbName, dumpFile);
     }
   }
 
@@ -91,7 +131,7 @@ public final class LiquibaseMigration {
 
   private void initDriver(String driver) {
     try {
-      Driver d = (Driver) Class.forName(driver).newInstance();
+      Driver d = (Driver) Class.forName(driver).getConstructor().newInstance();
       DriverManager.registerDriver(d);
     } catch (Exception e) {
       throw new LiquibaseUpdateException("jdbc driver class not found: " + driver, e);
