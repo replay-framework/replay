992e711a7c8879be6da3d2e8ed84ddadd79a8657
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-11-30 00:02:47 +0200

    make PrecompileTemplates smarter to ignore JS and XLS files

--- fastergt/src/play/modules/gtengineplugin/PrecompileTemplates.java
+++ fastergt/src/play/modules/gtengineplugin/PrecompileTemplates.java
@@ -1,8 +1,14 @@
 package play.modules.gtengineplugin;
 
 import play.Play;
+import play.PlayPlugin;
 import play.plugins.PluginCollection;
+import play.templates.Template;
 import play.templates.TemplateLoader;
+import play.vfs.VirtualFile;
+
+import java.util.Map;
+import java.util.Optional;
 
 public class PrecompileTemplates {
   public static void main(String[] args) {
@@ -25,9 +31,41 @@ public class PrecompileTemplates {
 
   private static class SinglePluginCollection extends PluginCollection {
     @Override public void loadPlugins() {
+      IgnoreOtherTemplatesPlugin otherTemplatesPlugin = new IgnoreOtherTemplatesPlugin();
+      otherTemplatesPlugin.index = 1;
+      addPlugin(otherTemplatesPlugin);
+
       GTEnginePlugin plugin = new GTEnginePlugin();
+      plugin.index = 2;
       plugin.onLoad();
       addPlugin(plugin);
+    }
+  }
+
+  private static class IgnoreOtherTemplatesPlugin extends PlayPlugin {
+    private final DummyTemplate dummyTemplate = new DummyTemplate();
+
+    @Override public Optional<Template> loadTemplate(VirtualFile file) {
+      if (file.getName().endsWith(".html") || file.getName().endsWith(".tag")) {
+        // will be precompiled by GTEnginePlugin
+        return Optional.empty();
+      }
+      if (file.getName().endsWith(".xls") || file.getName().endsWith(".js")) {
+        // no need to precompile
+        return Optional.of(dummyTemplate);
+      }
+
+      System.out.println("Unknown template type to precompile: " + file.getName());
+      return Optional.empty();
+    }
+  }
+
+  private static class DummyTemplate extends Template {
+    @Override public void compile() {
+    }
+
+    @Override protected String internalRender(Map<String, Object> args) {
+      throw new UnsupportedOperationException();
     }
   }
 }
