ade7afb5d02076258c7fa55c7c01e9513da0d80e
Andrei Solntsev, Margo Mitt <andrei@codeborne.com, margo@codeborne.com>
2019-11-29 14:20:56 +0200

    add missing tests for HttpRequest.getUserAgent()

--- framework/test/play/mvc/HttpRequestTest.java
+++ framework/test/play/mvc/HttpRequestTest.java
@@ -1,20 +1,18 @@
 package play.mvc;
 
 import org.junit.Test;
-import play.Play;
 import play.libs.Codec;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
+
+import static java.util.Collections.emptyMap;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestTest {
 
     @Test
     public void testBasicAuth() {
-
-        Play.configuration = new Properties();
-
         String encoded = Codec.encodeBASE64("username:pass:wordwithcolon");
         Http.Header header = new Http.Header("authorization", "Basic "+encoded);
         Map<String, Http.Header> headers = new HashMap<>();
@@ -22,21 +20,40 @@ public class HttpRequestTest {
 
         //This used to throw an exception if there was a colon in the password
         // test with currentRequest
-        Http.Request request = Http.Request.createRequest(
-                null,
-                "GET",
-                "/",
-                "",
-                null,
-                null,
-                null,
-                null,
-                false,
-                80,
-                "localhost",
-                false,
-                headers,
-                null
+        Http.Request request = request(headers);
+
+        assertThat(request.user).isEqualTo("username");
+        assertThat(request.password).isEqualTo("pass:wordwithcolon");
+    }
+
+    @Test
+    public void userAgent() {
+        Http.Request request = request(Map.of("user-agent", new Http.Header("user-agent", "Firefox 0.50-beta")));
+
+        assertThat(request.getUserAgent()).isEqualTo("Firefox 0.50-beta");
+    }
+
+    @Test
+    public void userAgent_unknown() {
+        assertThat(request(emptyMap()).getUserAgent()).isEqualTo("n/a");
+    }
+
+    private Http.Request request(Map<String, Http.Header> headers) {
+        return Http.Request.createRequest(
+          null,
+          "GET",
+          "/",
+          "",
+          null,
+          null,
+          null,
+          null,
+          false,
+          80,
+          "localhost",
+          false,
+          headers,
+          null
         );
     }
 }
