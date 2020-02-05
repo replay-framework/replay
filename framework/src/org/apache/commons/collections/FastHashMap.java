df562206f980bfc990fb207b41e31b9a3946f2df
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-02-05 18:48:14 +0200

    restore FastHashMap (from commons-collections) which is still used by commons-beanutils

---
+++ framework/src/org/apache/commons/collections/FastHashMap.java
@@ -0,0 +1,8 @@
+package org.apache.commons.collections;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FastHashMap<K, V> extends ConcurrentHashMap<K, V> {
+  public void setFast(boolean fast) {
+  }
+}
