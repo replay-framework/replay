d6de800964ad686560d85b1629f5949aed2145a6
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-07-23 02:07:03 +0300

    replay 1.8.0: upgrade to LiquiBase 4.0.0

---
+++ liquibase/test/play/modules/liquibase/PlayFileResourceAccessorTest.java
@@ -0,0 +1,19 @@
+package play.modules.liquibase;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayFileResourceAccessorTest {
+  private final PlayFileResourceAccessor accessor = new PlayFileResourceAccessor();
+
+  @Test
+  public void path_without_base() {
+    assertThat(accessor.getPath(null, "db/auth.xml")).isEqualTo("db/auth.xml");
+  }
+
+  @Test
+  public void path_with_base_file() {
+    assertThat(accessor.getPath("db/auth.xml", "users_view.sql")).isEqualTo("db/users_view.sql");
+  }
+}
