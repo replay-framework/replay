02ae971a6738f2294991a4b2e7071102f2c0f36c
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-01-13 01:00:19 +0200

    RePlay 1.8.6: load liquibase*.xsd files from classpath, not from network

--- liquibase/src/play/modules/liquibase/PlayFileResourceAccessor.java
+++ liquibase/src/play/modules/liquibase/PlayFileResourceAccessor.java
@@ -2,16 +2,23 @@ package play.modules.liquibase;
 
 import liquibase.resource.AbstractResourceAccessor;
 import liquibase.resource.InputStreamList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play.Play;
 import play.vfs.VirtualFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 import static java.util.stream.Collectors.toCollection;
 
 public class PlayFileResourceAccessor extends AbstractResourceAccessor {
+  private static final Logger logger = LoggerFactory.getLogger(PlayFileResourceAccessor.class);
+
   File findFile(String changeLogPath) {
     VirtualFile virtualFile = findVirtualFile(changeLogPath);
     if (virtualFile == null) throw new IllegalArgumentException("Changelog file not found: app/" + changeLogPath);
@@ -24,12 +31,27 @@ public class PlayFileResourceAccessor extends AbstractResourceAccessor {
 
   @Override public InputStreamList openStreams(String relativeTo, String streamPath) {
     String path = getPath(relativeTo, streamPath);
-    VirtualFile virtualFile = findVirtualFile(path);
     InputStreamList returnList = new InputStreamList();
+    VirtualFile virtualFile = findVirtualFile(path);
     if (virtualFile != null) {
       returnList.add(virtualFile.getURI(), virtualFile.inputstream());
     }
+    else {
+      findInClasspath(path, returnList);
+    }
     return returnList;
+  }
+
+  private void findInClasspath(String path, InputStreamList returnList) {
+    URL resource = Thread.currentThread().getContextClassLoader().getResource(path);
+    if (resource != null) {
+      try {
+        returnList.add(resource.toURI(), resource.openStream());
+      }
+      catch (URISyntaxException | IOException e) {
+        logger.error("Failed to read resource {}", resource, e);
+      }
+    }
   }
 
   String getPath(String relativeTo, String streamPath) {
