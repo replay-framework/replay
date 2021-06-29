592f3e56b1bdb845e700403ea8cd859c5445afcc
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-06-29 22:30:14 +0300

    #26 add integration tests for PDF generation

--- replay-tests/helloworld-kotlin/app/controllers/HelloWorld.kt
+++ replay-tests/helloworld-kotlin/app/controllers/HelloWorld.kt
@@ -1,10 +1,15 @@
 package controllers
 
+import play.modules.pdf.PdfResult
 import play.mvc.PlayController
 import play.rebel.View
 
 class HelloWorld : PlayController {
   fun hello(): View {
     return View("hello.html", mapOf("who" to "world"))
+  }
+
+  fun helloPdf(): PdfResult {
+    return PdfResult("hello.html").with("who", "PDF World on Kotlin")
   }
 }
