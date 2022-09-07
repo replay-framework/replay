a9e8031251dbec21c897af7460fd6ea433c42752
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-09-07 23:34:01 +0300

    add an example how to implement custom plugin for generating HTML for PDF

---
+++ replay-tests/helloworld/app/hello/plugins/PlainTextTemplate.java
@@ -0,0 +1,27 @@
+package hello.plugins;
+
+import play.templates.Template;
+import play.vfs.VirtualFile;
+
+import java.util.Map;
+
+public class PlainTextTemplate extends Template {
+  private final VirtualFile source;
+
+  public PlainTextTemplate(VirtualFile source) {
+    this.source = source;
+  }
+
+  @Override
+  public void compile() {
+  }
+
+  @Override
+  protected String internalRender(Map<String, Object> args) {
+    String result = source.contentAsString();
+    for (Map.Entry<String, Object> arg : args.entrySet()) {
+      result = result.replace("${" + arg.getKey() + "}", arg.getValue().toString());
+    }
+    return result;
+  }
+}
