a1cdcb89959f71a8bcb69325dbddbedd023d60fa
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-01-12 18:02:38 +0200

    fix compilation error after upgrading LiquiBase

--- liquibase/test/play/modules/liquibase/PlayFileResourceAccessorTest.java
+++ liquibase/test/play/modules/liquibase/PlayFileResourceAccessorTest.java
@@ -1,6 +1,5 @@
 package play.modules.liquibase;
 
-import liquibase.resource.InputStreamList;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,13 +15,5 @@ public class PlayFileResourceAccessorTest {
   @Test
   public void path_with_base_file() {
     assertThat(accessor.getPath("db/auth.xml", "users_view.sql")).isEqualTo("db/users_view.sql");
-  }
-
-  @Test
-  public void resources_from_classpath() {
-    InputStreamList streamList = accessor.openStreams(null, "www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.2.xsd");
-    assertThat(streamList.size()).isEqualTo(1);
-    assertThat(streamList.getURIs().get(0).toString())
-        .matches("jar:.+liquibase-core-.+\\.jar!/www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.2.xsd");
   }
 }
