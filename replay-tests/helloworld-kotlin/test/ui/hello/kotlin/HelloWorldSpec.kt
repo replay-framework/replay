0474497f31b91298bfbf7d7cb187dd0ac0cd455c
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-01-12 07:40:20 +0200

    add static file download test in Kotlin tests too

--- replay-tests/helloworld-kotlin/test/ui/hello/kotlin/HelloWorldSpec.kt
+++ replay-tests/helloworld-kotlin/test/ui/hello/kotlin/HelloWorldSpec.kt
@@ -2,12 +2,25 @@ package ui.hello.kotlin
 
 import com.codeborne.selenide.Condition
 import com.codeborne.selenide.Selenide
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FileUtils.readFileToString
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.io.IOException
+import java.net.URISyntaxException
 
 class HelloWorldSpec : BaseSpec() {
   @Test
   fun openHelloWorldPage() {
     Selenide.open("/")
     Selenide.element("h1").shouldHave(Condition.text("Hello, world!"))
+  }
+
+  @Test
+  fun openStaticFile() {
+    val downloadedFile = Selenide.download("/public/hello_world.txt", 4000)
+    assertThat(downloadedFile.name).isEqualTo("hello_world.txt")
+    assertThat(readFileToString(downloadedFile, "UTF-8")).isEqualTo("Hello, WinRar!")
   }
 }
