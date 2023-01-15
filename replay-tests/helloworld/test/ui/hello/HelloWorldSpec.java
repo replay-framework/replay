08ea3a2b36dceb97e0f8c84dfe56c838365489a3
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-01-16 00:57:39 +0200

    add UI test for images

--- replay-tests/helloworld/test/ui/hello/HelloWorldSpec.java
+++ replay-tests/helloworld/test/ui/hello/HelloWorldSpec.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import static com.codeborne.selenide.Condition.image;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -19,6 +20,13 @@ public class HelloWorldSpec extends BaseSpec {
   public void openHelloWorldPage() {
     open("/");
     $("h1").shouldHave(text("Hello, world!"));
+    $("#img1").shouldBe(image);
+    $("#img2").shouldBe(image);
+    $("#img3").shouldBe(image);
+    $("#img4").shouldNotBe(image);
+    $("#img5").shouldBe(image);
+    $("#img6").shouldBe(image);
+    $("#img7").shouldBe(image);
   }
 
   @Test
@@ -29,5 +37,13 @@ public class HelloWorldSpec extends BaseSpec {
             .isEqualTo("hello_world.txt");
     assertThat(readFileToString(downloadedFile, "UTF-8"))
             .isEqualTo("Hello, WinRar!");
+  }
+
+  @Test
+  public void openImage() throws IOException, URISyntaxException {
+    File downloadedFile = download("/img/favicon.png", 4000);
+
+    assertThat(downloadedFile.getName()).isEqualTo("favicon.png");
+    assertThat(downloadedFile).content().hasSize(707);
   }
 }
