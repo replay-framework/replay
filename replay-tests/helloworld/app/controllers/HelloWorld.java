9e89d8157016d1346ed13298aa2bbff911e5ffa0
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-04-09 01:09:53 +0300

    add integration tests for http status 400, 404, 500

--- replay-tests/helloworld/app/controllers/HelloWorld.java
+++ replay-tests/helloworld/app/controllers/HelloWorld.java
@@ -1,13 +1,28 @@
 package controllers;
 
 import com.google.common.collect.ImmutableMap;
+import play.data.validation.Min;
+import play.data.validation.Valid;
+import play.data.validation.Validation;
 import play.modules.pdf.PdfResult;
 import play.mvc.Controller;
+import play.mvc.results.BadRequest;
 import play.rebel.View;
 
 public class HelloWorld extends Controller {
   public View hello() {
     return new View("hello.html", ImmutableMap.of("who", "world"));
+  }
+
+  public View epicFail() {
+    return new View("epic-fail.html", ImmutableMap.of("who", "world"));
+  }
+
+  public View repeat(@Valid @Min(2) int times) {
+    if (Validation.hasErrors()) {
+      throw new BadRequest(Validation.errors().toString());
+    }
+    return new View("hello.html", ImmutableMap.of("who", "world #" + times));
   }
 
   public PdfResult helloPdf() {
