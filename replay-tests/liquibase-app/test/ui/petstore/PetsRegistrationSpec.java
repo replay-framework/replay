5d98276d0b9a4aa23c8b1fa52e89d70e2ce4400a
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-04-18 08:51:07 +0300

    add a demo application with JPA
    we need it to test following updates of JPA, Hibernate, validation and other dependencies.

---
+++ replay-tests/liquibase-app/test/ui/petstore/PetsRegistrationSpec.java
@@ -0,0 +1,30 @@
+package ui.petstore;
+
+import model.Kind;
+import org.junit.Test;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
+
+public class PetsRegistrationSpec extends BaseSpec {
+
+  @Test
+  public void canRegisterNewPet() {
+    open("/");
+    $$("#pets .pet").shouldHave(size(0));
+    $("#buttonRegisterPet")
+      .shouldHave(text("Register new pet"))
+      .click();
+
+    PetRegistrationPage page = page();
+    page.registerPet(Kind.COW, "Muuuuusie", 2);
+
+    $$("#pets .pet").shouldHave(size(1));
+    $("#totalCount").shouldHave(text("Total count: 1"));
+  }
+
+}
