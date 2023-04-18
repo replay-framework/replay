234f053ca5f4838b836a0c52c65e85170499f6ae
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-04-19 00:03:25 +0300

    working on RePlay 2.1.0

--- release.sh
+++ release.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env sh
+set -e
 
-./gradlew clean check publishToMavenLocal && ./gradlew publish --no-parallel --info
+version=$1
+
+if [ -z "$version" ] ; then
+  echo "Usage: ./release 2.1.0"
+  exit 0
+fi
+
+echo "Releasing version: ${version}"
+git tag -a "v${version}" -m "released replay ${version}"
+git push origin --tags
+
+./gradlew clean check 
+./gradlew uitest -Dselenide.headless=true 
+./gradlew publishToMavenLocal
+./gradlew publish --no-parallel --info
