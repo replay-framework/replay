6597fc289579225fd237ddb27819c984f3c72ee6
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-04-18 19:16:59 +0300

    add tests for Excel generation

---
+++ replay-tests/liquibase-app/app/controllers/PetReport.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import model.Pet;
+import model.PetRepository;
+import play.Play;
+import play.i18n.Lang;
+import play.i18n.Messages;
+import play.modules.excel.RenderExcel;
+import play.mvc.Controller;
+import play.mvc.results.Result;
+
+import javax.inject.Inject;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import static play.modules.excel.RenderExcel.RA_FILENAME;
+
+public class PetReport extends Controller {
+  private final PetRepository repository;
+
+  @Inject PetReport(PetRepository repository) {
+    this.repository = repository;
+  }
+
+  public Result report() {
+    List<Pet> pets = repository.loadAllPets();
+    Map<String, Object> args = Map.of(
+      "i18n", allMessages(),
+      "today", LocalDate.now(),
+      "pets", pets,
+      "petsCount", pets.size()
+    );
+    request.format = "xls";
+    renderArgs.put(RA_FILENAME, "pets-report.xls");
+    return new RenderExcel(Play.getVirtualFile("app/views/report.xls"), args, "pets-report.xls");
+  }
+
+  private Properties allMessages() {
+    String lang = Lang.get(request, response);
+    return Messages.all("en".equals(lang) ? "" : lang);
+  }
+}
---
+++ replay-tests/liquibase-app/test/ui/petstore/PetsReportSpec.java
@@ -0,0 +1,67 @@
+package ui.petstore;
+
+import model.Kind;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PetsReportSpec extends BaseSpec {
+
+  @Test
+  public void canRegisterNewPet() throws IOException, InvalidFormatException {
+    open("/");
+    $$("#pets .pet").shouldHave(size(0));
+    $("#buttonRegisterPet")
+      .shouldHave(text("Register new pet"))
+      .click();
+
+    PetRegistrationPage page = page();
+    page.registerPet(Kind.COW, "Muuuuusie", 2);
+
+    $$("#pets .pet").shouldHave(size(1));
+    $("#totalCount").shouldHave(text("Total count: 1"));
+    
+    // TODO generate new pets with API call
+    File report = $("#buttonReport").download();
+    verifyReportFile(report);
+  }
+
+  private void verifyReportFile(File report) throws IOException, InvalidFormatException {
+    assertThat(report.getName()).isEqualTo("pets-report.xls");
+
+    try (Workbook xls = WorkbookFactory.create(report)) {
+      assertThat(xls.getNumberOfSheets()).isEqualTo(1);
+      var cells = cellTexts(xls.getSheetAt(0));
+      assertThat(cells).contains("Pets store report", "Muuuuusie", "Total count: 1");
+    }
+
+  }
+
+  private List<String> cellTexts(Sheet sheet) {
+    List<String> cells = new ArrayList<>();
+    for (Row row : sheet) {
+      for (Cell cell : row) {
+        cells.add(cell.toString());
+      }
+    }
+    return cells;
+  }
+
+}
