replay-framework/replay

commit 02596f83a9831368429c69deae67d5d2384160ba

Make the error message more actionable

framework/src/play/server/Server.java
@@ -14,6 +14,9 @@ public class Server {
   }
 
   public int start() {
-    throw new IllegalStateException("Please add dependency replay-netty3, replay-netty4 or replay-javanet to your project");
+    throw new IllegalStateException(
+      "Please add the dependency com.codeborne.replay:javanet:netty3, com.codeborne.replay:netty4 or "
+      + "com.codeborne.replay:javanet to your project, and make sure it is declared before "
+      + "com.codeborne.replay:framework");
   }
 }

commit 788b79401e8c88c1a238afd8a9eca6e533300cb6

Typo was made around netty3 dependency in play.server.Server.start()
It is no "javanet:" between "replay:" and "netty3": "com.codeborne.replay:netty3"

Fixes: 02596f83a98 ("Make the error message more actionable")

framework/src/play/server/Server.java
@@ -15,7 +15,7 @@ public class Server {
 
   public int start() {
     throw new IllegalStateException(
-      "Please add the dependency com.codeborne.replay:javanet:netty3, com.codeborne.replay:netty4 or "
+      "Please add the dependency com.codeborne.replay:netty3, com.codeborne.replay:netty4 or "
       + "com.codeborne.replay:javanet to your project, and make sure it is declared before "
       + "com.codeborne.replay:framework");
   }