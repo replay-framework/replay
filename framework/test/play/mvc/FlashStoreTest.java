a1add414df9e2db7536645e071af13bf5974dddb
cies <ciesi-AT@stager-DOT.nl>
2023-11-09 09:25:15 +0100

    Throw errors (Forbidden) on flash cookie failures

--- framework/test/play/mvc/FlashStoreTest.java
+++ framework/test/play/mvc/FlashStoreTest.java
@@ -3,9 +3,11 @@ package play.mvc;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.jupiter.api.Test;
 import play.libs.Signer;
+import play.mvc.results.Forbidden;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -45,25 +47,25 @@ public class FlashStoreTest {
   }
 
   @Test
-  public void flash_restore_checksSignature() {
+  public void flash_restore_checksSignature_failure() {
       request.cookies.put("PLAY_FLASH", new Http.Cookie("PLAY_FLASH", "SIGNATURE-Zm9vPWJhcg=="));
       when(signer.isValid(anyString(), anyString())).thenReturn(false);
 
-      Scope.Flash flash = store.restore(request);
-      assertThat(flash.data).isEmpty();
-      assertThat(flash.out).isEmpty();
+      assertThatThrownBy(() -> store.restore(request))
+          .isInstanceOf(Forbidden.class)
+          .hasMessage("Invalid flash cookie signature");
 
       verify(signer).isValid("SIGNATURE", "Zm9vPWJhcg==");
   }
 
   @Test
-  public void flash_restore_oldFormatCookie() {
+  public void flash_restore_oldFormatCookie_thows_Forbidden() {
       request.cookies.put("PLAY_FLASH", new Http.Cookie("PLAY_FLASH", "Zm9vPWJhcg=="));
 
-      Scope.Flash flash = store.restore(request);
+    assertThatThrownBy(() -> store.restore(request))
+        .isInstanceOf(Forbidden.class)
+        .hasMessage("Flash cookie without signature");
 
-      assertThat(flash.data).isEmpty();
-      assertThat(flash.out).isEmpty();
       verifyNoMoreInteractions(signer);
   }
 }
