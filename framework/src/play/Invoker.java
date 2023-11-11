41720e30e1a66b6647f0d45741c8d4ab855a9a29
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-11-11 18:38:51 +0200

    init core pool size at least 4 threads by default
    It was never a good idea to use thread pool of size 1.
    It makes hang any PDF generation if html includes images from http://localhost...

--- framework/src/play/Invoker.java
+++ framework/src/play/Invoker.java
@@ -2,16 +2,21 @@ package play;
 
 import com.jamonapi.Monitor;
 import com.jamonapi.MonitorFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play.utils.PThreadFactory;
 
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
+
+import static java.lang.Integer.parseInt;
 
 /**
  * Run some code in a Play! context
  */
 public class Invoker {
+    private static final Logger log = LoggerFactory.getLogger(Invoker.class);
 
     /**
      * Main executor for requests invocations.
@@ -19,9 +24,14 @@ public class Invoker {
     public static ScheduledThreadPoolExecutor executor;
 
     static {
-        int core = Integer.parseInt(Play.configuration.getProperty("play.pool",
-          Play.mode == Play.Mode.DEV ? "1" : ((Runtime.getRuntime().availableProcessors() + 1) + "")));
-        executor = new ScheduledThreadPoolExecutor(core, new PThreadFactory("play"), new ThreadPoolExecutor.AbortPolicy());
+        String configuredPoolSize = Play.configuration.getProperty("play.pool");
+        int poolSize = configuredPoolSize != null ? parseInt(configuredPoolSize) : defaultPoolSize();
+        log.info("Replay thread pool size: {}, mode: {}", poolSize, Play.mode);
+        executor = new ScheduledThreadPoolExecutor(poolSize, new PThreadFactory("play"), new AbortPolicy());
+    }
+    
+    private static int defaultPoolSize() {
+        return Integer.max(Runtime.getRuntime().availableProcessors() + 1, 4);
     }
 
     /**
