9521de2e702fdfdc9e1a6a9cf96c60a271cd3343
cies <ciesi-AT@stager-DOT.nl>
2023-11-13 18:37:18 +0100

    Make Model.id field protected, improving getter/setter

--- framework/src/play/db/jpa/Model.java
+++ framework/src/play/db/jpa/Model.java
@@ -3,20 +3,25 @@ package play.db.jpa;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import javax.annotation.Nullable;
 
 /**
- * Base class for JPA model objects
- * Automatically provide a @Id Long id field
+ * Base class for JPA model objects.
+ * Automatically provides an @Id Long id field.
  */
 @MappedSuperclass
 public class Model extends GenericModel {
 
     @Id
     @GeneratedValue
-    public Long id;
+    protected Long id;
 
-    public Long getId() {
+    public @Nullable Long getId() {
         return id;
+    }
+
+    public void setId(@Nullable Long id) {
+        this.id = id;
     }
 
     @Override
