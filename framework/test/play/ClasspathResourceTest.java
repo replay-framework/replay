ac4764936ead93a11c8ded3fb9cf2884d4656e3d
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-11-21 19:15:51 +0200

    #198 verify that reading plugins descriptors from classpath is possible

--- framework/test/play/ClasspathResourceTest.java
+++ framework/test/play/ClasspathResourceTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ClasspathResourceTest {
   @Test
   void extractsNameOfJarFile_from_classpathURL() throws MalformedURLException {
-    assertThat(new ClasspathResource(new URL("jar:file:/Users/toomas/replay/build/libs/app-1.2.3.jar!/routes.yml")).getJarFilePath())
+    assertThat(new ClasspathResource("routes.yml", new URL("jar:file:/Users/toomas/replay/build/libs/app-1.2.3.jar!/routes.yml")).getJarFilePath())
       .isEqualTo("/Users/toomas/replay/build/libs/app-1.2.3.jar");
   }
 }
