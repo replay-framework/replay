821ced0fc6cf822931240afbe93ccca48de46aaa
Cies <cies-AT@stager-DOT.nl>
2024-03-25 16:54:32 +0100

    Make it build, and pass th test

--- framework/src/play/db/jpa/JPAPlugin.java
+++ framework/src/play/db/jpa/JPAPlugin.java
@@ -170,8 +170,8 @@ public class JPAPlugin extends PlayPlugin {
     protected PersistenceUnitInfoImpl persistenceUnitInfo(String dbName, Configuration dbConfig) {
         final List<Class> managedClasses = entityClasses(dbName);
         final Properties properties = properties(dbName, dbConfig);
-        properties.put(org.hibernate.jpa.AvailableSettings.LOADED_CLASSES, managedClasses);
-        properties.put(org.hibernate.jpa.AvailableSettings.FLUSH_MODE, MANUAL);
+        properties.put(AvailableSettings.LOADED_CLASSES, managedClasses);
+        properties.put(AvailableSettings.FLUSH_MODE, MANUAL);
         return new PersistenceUnitInfoImpl(dbName,
                 managedClasses, mappingFiles(dbConfig), properties);
     }
