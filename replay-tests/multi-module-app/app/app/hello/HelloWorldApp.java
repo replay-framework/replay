4a4376064b1d2989170214b3cbcabd29dd778dc3
Szabolcs Hubai <szab.hu@gmail.com>
2024-04-24 03:23:10 +0200

    Add "filePrefix" parameter to play.PropertiesConfLoader
    to ease migration from Play1 projects.
    
    Modify "multi-module-app" test to work from "conf/" directory:
    - use "conf/application.conf" for configuration
    - use "conf/routes" for routes
    
    This is an advanced setup, so play.server.Starter couldn't help.
    
    While at it, fix README about Criminals example project.
    
    Ref: #198

--- replay-tests/multi-module-app/app/app/hello/HelloWorldApp.java
+++ replay-tests/multi-module-app/app/app/hello/HelloWorldApp.java
@@ -1,10 +1,20 @@
 package hello;
 
+import play.ClasspathResource;
+import play.Play;
+import play.PropertiesConfLoader;
+import play.inject.DefaultBeanSource;
+import play.mvc.CookieSessionStore;
 import play.server.Starter;
 
 public class HelloWorldApp {
   public int start(String playId) {
-    return Starter.start(playId);
+    Play play = new Play(new PropertiesConfLoader("conf/"), new DefaultBeanSource(), new CookieSessionStore());
+    play.minimalInit(playId);
+    Play.routes = ClasspathResource.file("conf/routes");
+    Play.pluginCollection.loadPlugins();
+    play.start();
+    return Starter.start(play);
   }
   public static void main(String[] args) {
     new HelloWorldApp().start(System.getProperty("play.id", "prod"));
