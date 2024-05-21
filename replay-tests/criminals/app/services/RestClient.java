b469fc6808edcef42ad1b3599fc5d65824b0496e
Cies <cies-AT@stager-DOT.nl>
2024-05-21 15:37:29 +0200

    Fix tests
    Signed-off-by: Cies <cies-AT@stager-DOT.nl>

--- replay-tests/criminals/app/services/RestClient.java
+++ replay-tests/criminals/app/services/RestClient.java
@@ -2,16 +2,22 @@ package services;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import models.CriminalRecord;
 import org.apache.http.client.fluent.Request;
 
 import javax.inject.Singleton;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 @Singleton
 public class RestClient {
-  public  <T> List<T> get(String url) throws IOException {
+  public List<CriminalRecord> get(String url) throws IOException {
     String jsonResponse = Request.Get(url).execute().returnContent().asString();
-    return new Gson().fromJson(jsonResponse, new TypeToken<List<T>>() {}.getType());
+
+    Type criminalRecordType = new TypeToken<CriminalRecord>() {}.getType();
+    Type criminalRecordListType = TypeToken.getParameterized(ArrayList.class, criminalRecordType).getType();
+    return new Gson().fromJson(jsonResponse, criminalRecordListType);
   }
 }
