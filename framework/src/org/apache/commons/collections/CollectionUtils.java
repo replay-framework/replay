d400c930a2223321f76d40581cdbfacc818654d5
Cies <cies-AT@stager-DOT.nl>
2024-09-23 14:23:16 +0200

    Add docs the copied-in bits of commons-collections

--- framework/src/org/apache/commons/collections/CollectionUtils.java
+++ framework/src/org/apache/commons/collections/CollectionUtils.java
@@ -2,6 +2,10 @@ package org.apache.commons.collections;
 
 import java.util.Collection;
 
+// This is copied out of `org.apache.commons.collections`, since it is rather big and barely used.
+// Probably used by one of our dependencies (that now has the `transitive = false` flag set).
+
+@SuppressWarnings("unused") // Used indirectly, removing it makes test fail.
 public class CollectionUtils {
   public static <T> boolean isEmpty(Collection<T> collection) {
     return (collection == null || collection.isEmpty());
