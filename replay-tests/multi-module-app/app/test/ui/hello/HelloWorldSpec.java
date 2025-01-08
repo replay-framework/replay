ccf83f22c81f790a25e26e248a20efeb6930fca7
Szabolcs Hubai <szab.hu@gmail.com>
2025-01-09 00:52:02 +0100

    fastergt: gt_integration: resolve relative templates from classpath
    play.templates.TemplateLoader.loadTemplateFromClasspath() uses
    "Play.tmpDir" for extracting files from classpath.
    Let use that in this GTFileResolver.Resolver too, with
    the "view/" search prefix logic.

--- replay-tests/multi-module-app/app/test/ui/hello/HelloWorldSpec.java
+++ replay-tests/multi-module-app/app/test/ui/hello/HelloWorldSpec.java
@@ -4,8 +4,11 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.download;
 import static com.codeborne.selenide.Selenide.open;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
@@ -24,5 +27,13 @@ public class HelloWorldSpec extends BaseSpec {
 
     assertThat(downloadedFile.getName()).isEqualTo("favicon.png");
     assertThat(downloadedFile).hasDigest("MD5", "abd0852cbbbda55586f19e9aebb15d06");
+  }
+
+  @Test
+  public void openHelloWorldPageAndCheckMeta() throws URISyntaxException, IOException {
+    File helloFile = download("/", 4000);
+
+    assertThat(helloFile).content().containsIgnoringWhitespaces(IOUtils.toString(
+        requireNonNull(Thread.currentThread().getContextClassLoader().getResourceAsStream("views/meta.html")), UTF_8));
   }
 }
