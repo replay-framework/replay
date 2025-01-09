replay-framework/replay

commit cfe9ff10df568cfdf42a4e3b9f81dbf89124956c

load "errors/40x.html" templates from classpath
these files are located in "replay.jar:/errors/*.html"

framework/src/play/templates/TemplateLoader.java
@@ -6,10 +6,15 @@ import play.exceptions.TemplateCompilationException;
 import play.exceptions.TemplateNotFoundException;
 import play.vfs.VirtualFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static org.apache.commons.io.FileUtils.copyURLToFile;
 
 public class TemplateLoader {
 
@@ -202,10 +207,27 @@ public class TemplateLoader {
             VirtualFile tf = Play.getVirtualFile(path);
             if (tf != null && tf.exists()) {
                 template = TemplateLoader.load(tf);
-            } else {
-                throw new TemplateNotFoundException(path);
             }
         }
+        
+        if (template == null) {
+            URL resource = Play.classloader.getResource(path);
+            if (resource != null) {
+                File tmpTemplateFile = new File(Play.tmpDir, path);
+                try {
+                    copyURLToFile(resource, tmpTemplateFile);
+                }
+                catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                template = TemplateLoader.load(Play.getVirtualFile("tmp/" + path));
+            }
+        }
+
+        if (template == null) {
+            throw new TemplateNotFoundException(path);
+        }
+
         return template;
     }
 

commit 9970ca8866bfdcdebfddf65670bdffb1f94917f4

framework: Templateloader.loadTemplateFromClasspath() with custom tmpDir
When Play.tmpDir differs from the default "tmp", then loading templates
from the classpath fails miserably.
The "templateFile" variable should be loaded instead of '"tmp/" + path'.

Fixes: cfe9ff10df56 ("load "errors/40x.html" templates from classpath")

framework/src/play/templates/TemplateLoader.java
@@ -92,7 +92,7 @@ public class TemplateLoader {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    return TemplateLoader.load(Play.file("tmp/" + path));
+    return TemplateLoader.load(templateFile);
   }
 
   /**