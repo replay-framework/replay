replay-framework/replay

commit 4a4376064b1d2989170214b3cbcabd29dd778dc3

Add "filePrefix" parameter to play.PropertiesConfLoader
to ease migration from Play1 projects.

Modify "multi-module-app" test to work from "conf/" directory:
- use "conf/application.conf" for configuration
- use "conf/routes" for routes

This is an advanced setup, so play.server.Starter couldn't help.

While at it, fix README about Criminals example project.

Ref: #198

framework/src/play/PropertiesConfLoader.java
@@ -17,13 +17,25 @@ public class PropertiesConfLoader implements ConfLoader {
   private final Pattern overrideKeyPattern = Pattern.compile("^%([a-zA-Z0-9_\\-]+)\\.(.*)$");
   private final Pattern envVarInterpolationPattern = Pattern.compile("\\$\\{([^}]+)}");
 
+  private String filePrefix = "";
+
+  public PropertiesConfLoader() {
+    this("");
+  }
+
+  public PropertiesConfLoader(String filePrefix) {
+    if (filePrefix != null) {
+      this.filePrefix = filePrefix;
+    }
+  }
+
   public static Properties read(String playId) {
     return new PropertiesConfLoader().readConfiguration(playId);
   }
 
   @Override
   public Properties readConfiguration(String playId) {
-    return readOneConfigurationFile(playId, "application.conf");
+    return readOneConfigurationFile(playId, filePrefix + "application.conf");
   }
 
   public Properties readOneConfigurationFile(String playId, String filename) {
@@ -114,7 +126,7 @@ public class PropertiesConfLoader implements ConfLoader {
     for (Map.Entry<Object, Object> e : propsFromFile.entrySet()) {
       if (e.getKey().toString().startsWith("@include.")) {
         try {
-          String filenameToInclude = e.getValue().toString();
+          String filenameToInclude = filePrefix + e.getValue().toString();
           propsFromFile.putAll(readOneConfigurationFile(filenameToInclude, playId, inheritedId, confs));
         }
         catch (Exception ex) {

replay-tests/multi-module-app/app/app/hello/HelloWorldApp.java
@@ -1,10 +1,20 @@
 package hello;
 
+import play.ClasspathResource;
+import play.Play;
+import play.PropertiesConfLoader;
+import play.inject.DefaultBeanSource;
+import play.mvc.CookieSessionStore;
 import play.server.Starter;
 
 public class HelloWorldApp {
   public int start(String playId) {
-    return Starter.start(playId);
+    Play play = new Play(new PropertiesConfLoader("conf/"), new DefaultBeanSource(), new CookieSessionStore());
+    play.minimalInit(playId);
+    Play.routes = ClasspathResource.file("conf/routes");
+    Play.pluginCollection.loadPlugins();
+    play.start();
+    return Starter.start(play);
   }
   public static void main(String[] args) {
     new HelloWorldApp().start(System.getProperty("play.id", "prod"));

commit 3a4e324a31c6bae5d7d5309e1b1a8c2cb7ddfcfc

framework: use "filePrefix" for relative config files @include
Fixes: 4a4376064b1d ("Add "filePrefix" parameter to play.PropertiesConfLoader")

framework/src/play/PropertiesConfLoader.java
@@ -138,7 +138,8 @@ public class PropertiesConfLoader implements ConfLoader {
     for (Map.Entry<Object, Object> e : propsFromFile.entrySet()) {
       if (e.getKey().toString().startsWith("@include.")) {
         try {
-          String filenameToInclude = e.getValue().toString();
+          String value = e.getValue().toString();
+          String filenameToInclude = !value.startsWith("/") ? filePrefix + value : value;
           propsFromFile.putAll(
               readOneConfigurationFile(filenameToInclude, playId, inheritedId, confs));
         } catch (Exception ex) {