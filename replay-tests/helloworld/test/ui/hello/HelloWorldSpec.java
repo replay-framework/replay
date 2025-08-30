2f13fbd78bfb507363771c59b903e17549424a82
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-08-30 15:53:56 +0300

    #633 fix decoding query params containing plus
    Use `uri.getRawQuery()` which returns the initial query string as is - the following code will decode it.
    
    While `uri.getQuery()` returns already decoded query string where ' ' and '+' are both presented as '+' - which will be converted by the following code to ' '.

--- replay-tests/helloworld/test/ui/hello/HelloWorldSpec.java
+++ replay-tests/helloworld/test/ui/hello/HelloWorldSpec.java
@@ -12,4 +12,20 @@ public class HelloWorldSpec extends BaseSpec {
     open("/");
     $("h1").shouldHave(text("Hello, world!"));
   }
+
+  @Test
+  public void linkWithSpace() {
+    open("/");
+    $("h1").shouldHave(text("Hello, world!"));
+    $("#link-with-space").click();
+    $("h1").shouldHave(text("Hello, Tere morning!"));
+  }
+
+  @Test
+  public void linkWithPlus() {
+    open("/");
+    $("h1").shouldHave(text("Hello, world!"));
+    $("#link-with-plus").click();
+    $("h1").shouldHave(text("Hello, You+me!"));
+  }
 }
